ccae959f0f25788356527054598dfcb3956e5b0a
TenSt <smaksymc@redhat.com>
2026-03-04 14:41:35 +0100

    return back FetchGPGKey

---
+++ pkg/yum/gpg_key.go
@@ -0,0 +1,35 @@
+package yum
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/ProtonMail/go-crypto/openpgp"
+)
+
+// FetchGPGKey GETs GPG Key from url with request timeout maximum timeout.
+func FetchGPGKey(ctx context.Context, url string, client *http.Client) (*string, int, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return nil, 0, fmt.Errorf("error creating request: %w", err)
+	}
+	resp, err := client.Do(req) // #nosec G704
+	if err != nil {
+		return nil, 0, err
+	}
+	defer resp.Body.Close()
+	code := resp.StatusCode
+	gpgKeyString, err := responseBodyToString(resp.Body)
+	if err != nil {
+		return nil, 0, err
+	}
+	if err == nil && code < 200 || code > 299 {
+		return nil, code, fmt.Errorf("received http %d", code)
+	}
+	if _, openpgpErr := openpgp.ReadArmoredKeyRing(strings.NewReader(*gpgKeyString)); err != nil {
+		return nil, code, openpgpErr //Bad key
+	}
+	return gpgKeyString, code, nil
+}
